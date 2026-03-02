dd5d2a6d78890ca58a183d76d0e46d35aa60d4ba
rrc-w2025-cnagtalon <cnagtalon@academic.rrc.ca>
2026-03-01 23:10:57 -0600

    Added validate file and modified create route for to use validation.

--- src/api/v1/routes/eventRoutes.ts
+++ src/api/v1/routes/eventRoutes.ts
@@ -1,9 +1,11 @@
 import express, { Router } from "express";
 import { createEvent, deleteEvent, getAllEvent, getEventById, updateEvent } from "../controllers/eventController";
+import { validateRequest } from "../middleware/validate";
+import { eventSchemas } from "../validation/eventsSchemas";
 
 const eventsRoutes: Router = express.Router();
 
-eventsRoutes.post("/", createEvent);
+eventsRoutes.post("/", validateRequest(eventSchemas.create), createEvent);
 eventsRoutes.get("/:id", getEventById);
 eventsRoutes.get("/", getAllEvent);
 eventsRoutes.put("/:id", updateEvent);
