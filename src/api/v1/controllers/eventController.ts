be3f7fde3eb166c885f8e2982f63419920aafd1a
rrc-w2025-cnagtalon <cnagtalon@academic.rrc.ca>
2026-03-01 20:48:10 -0600

    Created getAllEvent function in controller file.

--- src/api/v1/controllers/eventController.ts
+++ src/api/v1/controllers/eventController.ts
@@ -1,6 +1,6 @@
 import { Request, RequestHandler, Response } from "express";
 import { EventCreateRequest } from "../models/eventCreateRequestModel"
-import { createNewEvent, getEventByIdAsync } from "../services/eventService"
+import { createNewEvent, getEventByIdAsync, getAllEvents } from "../services/eventService"
 import { HTTP_STATUS } from "../../../constants/httpConstants"
 import { successResponse } from "../models/responseModel"
 
@@ -32,4 +32,15 @@ export const getEventById = async (req: Request, res: Response) => {
     } catch (error) {
         res.status(HTTP_STATUS.NOT_FOUND).json({message: "Nope error." });
     }
+};
+
+export const getAllEvent = async (req: Request, res: Response) => {
+    // Logic to get all items
+    try {
+        const items = await getAllEvents();
+
+        res.status(HTTP_STATUS.OK).json(successResponse(items, "Here are all the events."));
+    } catch (error) {
+        res.status(HTTP_STATUS.INTERNAL_SERVER_ERROR).json({message: "Internal server error"});
+    }  
 };
