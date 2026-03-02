00a60837de88a63c3b74392a0edb43c30a8070bf
rrc-w2025-cnagtalon <cnagtalon@academic.rrc.ca>
2026-03-01 20:24:39 -0600

    Added route for getting event by ID.

--- src/api/v1/routes/eventRoutes.ts
+++ src/api/v1/routes/eventRoutes.ts
@@ -1,8 +1,9 @@
 import express, { Router } from "express";
-import { createEvent } from "../controllers/eventController";
+import { createEvent, getEventById } from "../controllers/eventController";
 
 const eventsRoutes: Router = express.Router();
 
 eventsRoutes.post("/", createEvent);
+eventsRoutes.get("/", getEventById);
 
 export default eventsRoutes;
