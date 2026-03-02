6580c420643a765d2964fe196ee2deff45564cba
rrc-w2025-cnagtalon <cnagtalon@academic.rrc.ca>
2026-03-01 20:20:43 -0600

    Created EventResponse model, and getEventByIdAsync function in service file.

--- src/api/v1/services/eventService.ts
+++ src/api/v1/services/eventService.ts
@@ -1,7 +1,23 @@
-import { addDocument } from "../repositories/eventRepository"
+import { addDocument, getDocumentById } from "../repositories/eventRepository"
 import { EventCreateRequest } from "../models/eventCreateRequestModel"
+import { EventResponse } from "../models/eventResponse"
 
 export const createNewEvent = async (event: EventCreateRequest): Promise<string> => {
     return await addDocument(event);
 
+};
+
+export const getEventByIdAsync = async (id: string): Promise<EventResponse> => {
+    let entity = await getDocumentById(id);
+    return {
+       id: entity?.id,
+       name: entity?.name,
+       date: entity?.date,
+       capacity: entity?.capacity,
+       registrationCount: entity?.registrationCount,
+       status: entity?.status,
+       category: entity?.category,
+       createdAt: entity?.createdAt,
+       updatedAt: entity?.updatedAt
+    }
 };
