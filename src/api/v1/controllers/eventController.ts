367265fad4ff7a839ac575d5b64ffd803f1fa958
rrc-w2025-cnagtalon <cnagtalon@academic.rrc.ca>
2026-03-01 20:23:38 -0600

    Created getEventById function in controller file.

--- src/api/v1/controllers/eventController.ts
+++ src/api/v1/controllers/eventController.ts
@@ -1,6 +1,6 @@
 import { Request, RequestHandler, Response } from "express";
 import { EventCreateRequest } from "../models/eventCreateRequestModel"
-import { createNewEvent } from "../services/eventService"
+import { createNewEvent, getEventByIdAsync } from "../services/eventService"
 import { HTTP_STATUS } from "../../../constants/httpConstants"
 import { successResponse } from "../models/responseModel"
 
@@ -22,3 +22,14 @@ export const createEvent = async (req: Request, res: Response) => {
     res.status(HTTP_STATUS.CREATED).json(successResponse(result, "Event created"));
 };
 
+// get an item
+export const getEventById = async (req: Request, res: Response) => {
+    try {
+        let id = req.params.id;
+        let results = await getEventByIdAsync(id);
+
+        res.status(HTTP_STATUS.OK).json(successResponse(results, "Data retrieved."));
+    } catch (error) {
+        res.status(HTTP_STATUS.NOT_FOUND).json({message: "Nope error." });
+    }
+};
