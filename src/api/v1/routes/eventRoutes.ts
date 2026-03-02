beb17c451aa6b15f974cd4e9048b66e04be8607b
rrc-w2025-cnagtalon <cnagtalon@academic.rrc.ca>
2026-03-01 21:06:35 -0600

    Added route for updating an event.

--- src/api/v1/routes/eventRoutes.ts
+++ src/api/v1/routes/eventRoutes.ts
@@ -1,10 +1,11 @@
 import express, { Router } from "express";
-import { createEvent, getAllEvent, getEventById } from "../controllers/eventController";
+import { createEvent, getAllEvent, getEventById, updateEvent } from "../controllers/eventController";
 
 const eventsRoutes: Router = express.Router();
 
 eventsRoutes.post("/", createEvent);
 eventsRoutes.get("/:id", getEventById);
 eventsRoutes.get("/", getAllEvent);
+eventsRoutes.put("/:id", updateEvent);
 
 export default eventsRoutes;
